b88c8a41a6bedde87d559fc556d1d5fe280ceeb4
punmagarsumit-nwsl <s-punmagar@new-wave.jp>
2025-02-06 17:07:09 +0900

    build error

--- pages/about.vue
+++ pages/about.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { aboutPage, footerData, navbarData } from '~/data'
+import { aboutPage, footerData } from '~/data'
 
 useHead({
   title: 'About',
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,4 @@
 <script lang="ts" setup>
-import { navbarData } from '~/data'
 
 useHead({
   title: 'Home',
